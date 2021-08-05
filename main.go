8f6e3ff82098f50076a1b44d60ab99863ed3f989
Andrei Yangabishev <waffleboot@gmail.com>
2021-08-06 00:27:17 +0300

    plus minus 2

--- main.go
+++ main.go
@@ -52,31 +52,69 @@ func plus_minus(n int) string {
 	return "not possible"
 }
 
-func plus_minus_2(n int) string {
-	digits := split(n)
-	var buf strings.Builder
-	sum := digits[0]
-	// fmt.Println(sum)
-	for i := 1; i < len(digits); i++ {
-		if sum >= 0 {
-			sum -= digits[i]
-			buf.WriteRune('-')
-		} else {
-			sum += digits[i]
-			buf.WriteRune('+')
+func plus_minus2(n int) string {
+
+	const mid = 100
+
+	nums := split(n)
+
+	dp := make([][]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		dp[i] = make([]int, 2*mid)
+	}
+
+	dp[0][mid+nums[0]] = 1
+
+	for i := 1; i < len(nums); i++ {
+		for sum := -mid; sum < mid; sum++ {
+			if dp[i-1][mid+sum] != 0 {
+				if dp[i][mid+sum+nums[i]] == 0 {
+					dp[i][mid+sum+nums[i]] = 1
+				}
+				dp[i][mid+sum-nums[i]] = -1
+			}
 		}
-		// fmt.Println(sum)
 	}
-	if sum == 0 {
-		return buf.String()
+
+	for i := 0; i < len(nums); i++ {
+		var buf strings.Builder
+		for j := 0; j < len(dp[i]); j++ {
+			if dp[i][j] > 0 {
+				buf.WriteRune('+')
+			} else if dp[i][j] < 0 {
+				buf.WriteRune('-')
+			} else {
+				buf.WriteRune(' ')
+			}
+		}
+		fmt.Println(buf.String())
 	}
-	return "not possible"
+
+	if dp[len(nums)-1][mid+0] == 0 {
+		return "not possible"
+	}
+
+	var sum int
+	ans := make([]rune, len(nums)-1)
+	for i := len(nums) - 1; i > 0; i-- {
+		if dp[i][mid+sum] < 0 {
+			ans[i-1] = '-'
+			sum -= nums[i]
+		} else {
+			ans[i-1] = '+'
+			sum += nums[i]
+		}
+	}
+
+	return string(ans)
+
 }
 
 func main() {
-	// n := 11
+	// n := 2234
+	n := 11211
 	// n := 123123123123
-	n := 9223372036854775807
+	// n := 9223372036854775807
 	fmt.Println(plus_minus(n))
-	// fmt.Println(plus_minus_2(n))
+	fmt.Println(plus_minus2(n))
 }
