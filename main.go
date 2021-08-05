553c7afb2873ebbe29da79db3171744f906c3b7d
Andrei Yangabishev <waffleboot@gmail.com>
2021-08-06 02:26:12 +0300

    написал оптимальный код

--- main.go
+++ main.go
@@ -51,57 +51,102 @@ func plus_minus(n int) string {
 	return "not possible"
 }
 
-func plus_minus2(n int) string {
-
-	const mid = 100
-
-	nums := split(n)
-
-	dp := make([][]bool, len(nums))
-	for i := 0; i < len(nums); i++ {
-		dp[i] = make([]bool, 2*mid)
-	}
-
-	dp[0][mid+nums[0]] = true
-
-	for i := 1; i < len(nums); i++ {
-		for sum := -mid; sum < mid; sum++ {
-			if dp[i-1][mid+sum] {
-				dp[i][mid+sum+nums[i]] = true
-				dp[i][mid+sum-nums[i]] = true
+func margin(dp [][]int) (int, int) {
+	l, r := len(dp[0]), 0
+	for i := 0; i < len(dp); i++ {
+		for j := 0; j < len(dp[i]); j++ {
+			if dp[i][j] > 0 {
+				if j < l {
+					l = j
+				}
+				if r < j {
+					r = j
+				}
 			}
 		}
 	}
+	return l, r
+}
 
-	for i := 0; i < len(nums); i++ {
+func backtrack(nums []int, mid int, dp [][]int) {
+	l, r := margin(dp)
+	dp[len(nums)-1][mid+0] = 2
+	for i := len(nums) - 2; i >= 0; i-- {
+		for j := l; j <= r; j++ {
+			if dp[i][j] == 1 {
+				if dp[i+1][j+nums[i+1]] == 2 {
+					dp[i][j] = 2
+				} else if dp[i+1][j-nums[i+1]] == 2 {
+					dp[i][j] = 2
+				}
+			}
+		}
+	}
+}
+
+func path(nums []int, mid int, dp [][]int) string {
+	sum := nums[0]
+	ans := make([]rune, len(nums)-1)
+	for i := 1; i < len(nums); i++ {
+		if dp[i][mid+sum-nums[i]] == 2 {
+			ans[i-1] = '-'
+			sum -= nums[i]
+		} else {
+			ans[i-1] = '+'
+			sum += nums[i]
+		}
+
+	}
+	return string(ans)
+}
+
+func dump(dp [][]int, all bool) {
+	l, r := margin(dp)
+	for i := 0; i < len(dp); i++ {
 		var buf strings.Builder
-		for j := 0; j < len(dp[i]); j++ {
-			if dp[i][j] {
-				buf.WriteRune('+')
+		for j := l; j <= r; j++ {
+			if dp[i][j] == 2 {
+				buf.WriteRune('2')
+			} else if dp[i][j] == 1 && all {
+				buf.WriteRune('1')
 			} else {
 				buf.WriteRune(' ')
 			}
 		}
 		fmt.Println(buf.String())
 	}
+	fmt.Println()
+}
 
-	if !dp[len(nums)-1][mid+0] {
-		return "not possible"
+func plus_minus2(n int) string {
+
+	const mid = 100
+
+	nums := split(n)
+
+	dp := make([][]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		dp[i] = make([]int, 2*mid)
 	}
 
-	var sum int
-	ans := make([]rune, len(nums)-1)
-	for i := len(nums) - 1; i > 0; i-- {
-		if dp[i-1][mid+sum-nums[i]] {
-			ans[i-1] = '+'
-			sum -= nums[i]
-		} else {
-			ans[i-1] = '-'
-			sum += nums[i]
+	dp[0][mid+nums[0]] = 1
+
+	for i := 1; i < len(nums); i++ {
+		for sum := -mid; sum < mid; sum++ {
+			if dp[i-1][mid+sum] > 0 {
+				dp[i][mid+sum+nums[i]] = 1
+				dp[i][mid+sum-nums[i]] = 1
+			}
 		}
 	}
 
-	return string(ans)
+	if dp[len(nums)-1][mid+0] == 0 {
+		return "not possible"
+	}
+
+	backtrack(nums, mid, dp)
+
+	return path(nums, mid, dp)
 
 }
 
@@ -109,8 +154,10 @@ func main() {
 	// n := 2234
 	// n := 11211
 	// n := 123123123123
-	// n := 9223372036854775807
-	n := 9223372031
+	n := 9223372036854775807
+	// n := 9223372031
+	// n := 9223372031
+	// fmt.Println(n)
 	fmt.Println(plus_minus(n))
 	fmt.Println(plus_minus2(n))
 }
--- main_test.go
+++ main_test.go
@@ -9,3 +9,9 @@ func BenchmarkPlusMinus(b *testing.B) {
 		x = plus_minus(9223372036854775807)
 	}
 }
+
+func BenchmarkPlusMinus2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		x = plus_minus2(9223372036854775807)
+	}
+}
